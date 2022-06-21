ba296fef0d066cb2c39b8fe24e2419e8ca8a12ca
Harinithya <73685642+HariNithyaRao@users.noreply.github.com>
2022-06-21 12:11:23 +0530

    Add files via upload

---
+++ WordCloud.py
@@ -0,0 +1,21 @@
+def calculate_frequencies(s):
+    cloud = w.WordCloud()
+    cloud.generate_from_frequencies(Counter(s))
+    return cloud.to_array()
+
+import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+import wordcloud as w
+import io
+import sys
+from collections import Counter
+
+st.title("WElCOME TO WORDCLOUD🖼️")
+st.write("Enter some words and have fun with wordcloud 😁")
+txt = st.text_input("Type your text here!")
+if len(txt) != 0:
+    # generate cloud
+    st.balloons()
+    st.image(calculate_frequencies(txt.split()))
+
