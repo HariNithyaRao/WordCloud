9ab95573b677d5971e8473cf9e85b2e3a23423a1
Harinithya <73685642+HariNithyaRao@users.noreply.github.com>
2023-05-13 16:49:38 +0530

    Update WordCloud_Streamlitapp.py

--- WordCloud_Streamlitapp.py
+++ WordCloud_Streamlitapp.py
@@ -3,6 +3,27 @@ import numpy as np
 import wordcloud 
 from collections import Counter
 
+import streamlit as st
+
+# Define some CSS styles
+css = """
+<style>
+body {
+  animation: colorchange 50s infinite;
+}
+
+@keyframes colorchange
+{
+  0%   {background: #FFC0CB;}
+  25%  {background: #90EE90;}
+  50%  {background: #ADD8E6;}
+  75%  {background: #FFA07A;}
+  100% {background: #FFC0CB;}
+}
+</style>
+"""
+st.write(css, unsafe_allow_html=True)
+
 def calculate_frequencies(s):
     cloud = wordcloud.WordCloud()
     cloud.generate_from_frequencies(Counter(s))
