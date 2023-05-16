ec0d9ab1d451aff5a6ac448270f6ada1a645b969
Harinithya <73685642+HariNithyaRao@users.noreply.github.com>
2023-05-16 12:34:33 +0530

    Update WordCloud_Streamlitapp.py

--- WordCloud_Streamlitapp.py
+++ WordCloud_Streamlitapp.py
@@ -3,31 +3,23 @@ import numpy as np
 import wordcloud 
 from collections import Counter
 
-# Define some CSS styles
-css = """
+page_bg_img = '''
 <style>
 body {
-  animation: colorchange 50s infinite;
-}
-
-@keyframes colorchange
-{
-  0%   {background: #FFC0CB;}
-  25%  {background: #90EE90;}
-  50%  {background: #ADD8E6;}
-  75%  {background: #FFA07A;}
-  100% {background: #FFC0CB;}
+background-image: url("https://images.unsplash.com/photo-1683010606819-de5d37be0b25?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1975&q=80");
+background-size: cover;
 }
 </style>
-"""
-st.markdown(css, unsafe_allow_html=True)
+'''
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
 
 def calculate_frequencies(s):
     cloud = wordcloud.WordCloud()
     cloud.generate_from_frequencies(Counter(s))
     return cloud.to_array()
 
-st.title("WElCOME TO WORDCLOUD🖼️")
+st.title("WELCOME TO WORDCLOUD🖼️")
 st.write("Enter some words and have fun with wordcloud 😁")
 txt = st.text_input("Type your text here!")
 if len(txt) != 0:
